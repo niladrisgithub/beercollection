49188ae13b3355b16e6109f6ef51e49b45bb124f
Niladri <niladri.sinha89@gmail.com>
2021-06-30 23:18:11 -0400

    wip: create model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Beer(models.Model):
+    brewery_name = models.CharField(max_length=100)
+    beer_name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    type = models.CharField(max_length=15)
+    abv = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+  
