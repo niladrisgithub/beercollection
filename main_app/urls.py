d3aac7191692268c18d3ee321f6344f3ef37e7e3
Niladri <niladri.sinha89@gmail.com>
2021-07-06 23:48:14 -0400

    function: creates beer

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('beers/', views.beers_index, name='index'),
     path('beers/<int:beer_id>/', views.beers_detail, name='detail'),
+    path('beers/create/', views.BeerCreate.as_view(), name='beers_create'),
+    # path('beers/int:pk/update/', views.BeerUpdate.as_view(), name='beers_update'),
+    # path('beers/int:pk/delete/', views.BeerDelete.as_view(), name='beers_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.shortcuts import render
 from .models import Beer
 
@@ -10,9 +11,15 @@ def about(request):
     return render(request, 'about.html')
 
 def beers_index(request):
-    beers = Beer.objects.all().order_by('-abv')
+    beers = Beer.objects.all().order_by('abv')
     return render(request, 'beers/index.html', {'beers': beers})
 
 def beers_detail(request, beer_id):
     beer = Beer.objects.get(id=beer_id)
     return render(request, 'beers/detail.html', {'beer': beer})
+
+class BeerCreate(CreateView):
+    model = Beer
+    fields = '__all__'
+    success_url = '/beers/'
+
