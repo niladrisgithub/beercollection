7f45e6b5827e92caa6d8b9590c4f3b2ca2cd5192
Niladri <niladri.sinha89@gmail.com>
2021-07-08 00:59:21 -0400

    wip: hops load, but no fields to into Add Hop

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Beer, Hop
+from .models import Beer, Hop, Drinking
 
 admin.site.register(Beer)
 admin.site.register(Hop)
+admin.site.register(Drinking)
