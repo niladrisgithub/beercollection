f3ca1f70d8c1b129a85fd27fd90c18a1a121e898
Niladri <niladri.sinha89@gmail.com>
2021-07-10 18:48:22 -0400

    wip: M:M relationship

--- main_app/models.py
+++ main_app/models.py
@@ -10,12 +10,28 @@ SERVED = (
     ('G', 'Growler'),
 )
 
+
+class Hop(models.Model):
+    name: models.CharField(max_length=15)
+    characteristics: models.CharField(max_length=60)
+    alpha_acid: models.DecimalField(max_digits=5, decimal_places=2)
+    color: models.CharField(max_length=5, default='Green')
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'hop_id': self.id})
+
 class Beer(models.Model):
     brewery_name = models.CharField(max_length=100)
     beer_name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     type = models.CharField(max_length=15)
     abv = models.DecimalField(max_digits=5, decimal_places=2)
+    # add M:M relationship
+    hops = models.ManyToManyField(Hop)
 
     def __str__(self):
         return(self.beer_name)
@@ -24,17 +40,6 @@ class Beer(models.Model):
         return reverse('detail', kwargs={'beer_id': self.id})
 
 
-class Hop(models.Model):
-    name: models.CharField(max_length=20)
-    characteristics: models.CharField(max_length=50)
-    alpha_acid: models.DecimalField(max_digits=5, decimal_places=2)
-
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'hop_id': self.id})
 
 
 class Drinking(models.Model):
